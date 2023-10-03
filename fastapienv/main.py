9e149a131df596d9e6cf214b40658433fb368e6c
Lautaro <ldelucia@fi.uba.ar>
2023-10-03 09:38:33 -0300

    Creo los endpoints de reportes

--- fastapienv/main.py
+++ fastapienv/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pymongo import MongoClient
 import os
 # import database utilities
@@ -12,6 +12,8 @@ from PIL import Image
 from io import BytesIO
 import base64
 # import project utilities
+from datetime import datetime, timedelta
+from dateutil import parser
 import logging 
 import db
 import os
@@ -57,35 +59,99 @@ async def get_reports_for_test(test_name: str):
 @app.get("/reports/{test_name}/{student_email}/screenshot")
 async def get_screenshot_details(test_name: str, student_email: str):
     try:
-        pass  # Placeholder for actual implementation
+        # Constructing the query to find the document that matches the given test_name and student_email
+        query = {"exam": test_name, "student": student_email}
+        # Retrieving the entry from the screenshot collection in the database
+        screenshot_entries = db.get_mongo_collection("screenshot", query)        
+        # If there are no entries that match the query, return the status message
+        if not screenshot_entries:
+            return "The student sent no screenshot"
+        # Assuming there is only one entry that matches the query, return the image field of the entry
+        return screenshot_entries[0]["image"]
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 @app.get("/reports/{test_name}/{student_email}/out_of_frame")
 async def get_out_of_frame_details(test_name: str, student_email: str):
     try:
-        pass  # Placeholder for actual implementation
+        out_of_frame_entries = db.get_mongo_collection("outOfFrame", {"exam": test_name, "student": student_email})
+        final_data = []
+        
+        # Retrieving all corresponding images from the periodicPhotos collection in the database for the given test_name and student_email
+        all_periodic_photos_entries = db.get_mongo_collection("periodicPhotos", {"exam": test_name, "student": student_email})
+        
+        for entry in out_of_frame_entries:
+            time_str = entry.get("time")
+            duration = entry.get("duration")
+            time_dt = parser.parse(time_str)
+            end_time_dt = time_dt + timedelta(seconds=duration)
+            
+            # Filtering the periodic_photos_entries based on the time frame
+            filtered_periodic_photos_entries = [
+                photo_entry for photo_entry in all_periodic_photos_entries
+                if time_dt <= parser.parse(photo_entry.get("time")) <= end_time_dt
+            ]
+            
+            # Extracting the base64 images from the filtered entries
+            images_base64 = [photo_entry.get("image") for photo_entry in filtered_periodic_photos_entries]
+            
+            final_data.append({
+                "time": time_str,
+                "duration": duration,
+                "images": images_base64
+            })
+        
+        return final_data
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 @app.get("/reports/{test_name}/{student_email}/blur")
 async def get_blur_details(test_name: str, student_email: str):
     try:
-        pass  # Placeholder for actual implementation
+        # Constructing the query to find the documents that match the given test_name and student_email in blur collection
+        query = {
+            "student": student_email,
+            "exam": test_name
+        }
+        logger.log(f"Connecting to blur database for student: {student_email}", logging.INFO)
+        # Retrieving the corresponding entries from the blur collection in the database
+        blur_entries = db.get_mongo_collection("blur", query)
+        logger.log(f"Retrieving blur entries: {blur_entries}", logging.INFO)
+        # Extracting the time and message from each entry and returning them
+        return [{"time": entry["time"], "msg": entry["msg"]} for entry in blur_entries]
+        
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 @app.get("/reports/{test_name}/{student_email}/object_detection")
 async def get_object_detection_details(test_name: str, student_email: str):
     try:
-        pass  # Placeholder for actual implementation
+        # Constructing the query to find the documents that match the given test_name and student_email in blur collection
+        query = {
+            "student": student_email,
+            "exam": test_name
+        }
+        logger.log(f"Connecting to periodicPhotos database for student: {student_email}", logging.INFO)
+        # Retrieving the corresponding entries from the blur collection in the database
+        photo_entries = db.get_mongo_collection("periodicPhotos", query)
+        # Extracting the time and message from each entry and returning them
+        return [{"time": entry["time"], "image": entry["image"]} for entry in photo_entries]
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 @app.get("/reports/{test_name}/{student_email}/speech_detection")
 async def get_speech_detection_details(test_name: str, student_email: str):
     try:
-        pass  # Placeholder for actual implementation
+        # Constructing the query to find the documents that match the given test_name and student_email in blur collection
+        query = {
+            "student": student_email,
+            "exam": test_name
+        }
+        logger.log(f"Connecting to speech database for student: {student_email}", logging.INFO)
+        # Retrieving the corresponding entries from the blur collection in the database
+        speech_entries = db.get_mongo_collection("conversations", query)
+        # Extracting the time and message from each entry and returning them
+        return [{"time": entry["time"], "conversation": entry["conversation"]} for entry in speech_entries]
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
